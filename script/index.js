0b6d2a945897879c23f41fa9894d71f1e0c47861
VAbrishamii <vahide.abri@yahoo.com>
2024-01-30 21:56:53 +0100

    Commit

--- script/index.js
+++ script/index.js
@@ -1,10 +1,16 @@
-import  {getData}  from "./module/httprequest"
+// import  {getData}  from "./module/httprequest"
 
+// const init= async() => {
+//     const allProducts = await getData `All entries` ;
+//     console.log(allProducts)
 
-const init= async() => {
-    const allProducts = await getData `All entries` ;
-    console.log(allProducts)
+// };
 
-};
+// document.addEventListener("DOMContentLoaded", init);
 
-document.addEventListener("DOMContentLoaded", init);
+async function getData() {
+  const res = await fetch("https://docs.noroff.dev");
+  const data = await res.json();
+  console.log(data);
+}
+getData();
