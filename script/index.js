611a7713bf76bb47735fd3c1048d74f089093629
VAbrishamii <vahide.abri@yahoo.com>
2024-01-31 20:26:45 +0100

    Commit

--- script/index.js
+++ script/index.js
@@ -1,16 +1,24 @@
-// import  {getData}  from "./module/httprequest"
-
-// const init= async() => {
-//     const allProducts = await getData `All entries` ;
-//     console.log(allProducts)
-
-// };
-
-// document.addEventListener("DOMContentLoaded", init);
-
-async function getData() {
-  const res = await fetch("https://docs.noroff.dev");
-  const data = await res.json();
-  console.log(data);
+const url = "https://api.noroff.dev/api/v1/rainy-days";
+function displayPosts(post) {
+  for (let i = 0; i < post.length; i++) {
+    const title = post[i].title;
+    const gender = post[i].gender;
+    // Call a function to generate HTML
+    console.log("Title:", title, "Gender:", gender);
+  }
 }
-getData();
+async function doFetch(url) {
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.log("Error", error);
+  }
+}
+
+async function main() {
+  const posts = await doFetch(url);
+  displayPosts(posts);
+}
+main();
--- script/module/httprequest.js
+++ script/module/httprequest.js
@@ -1,13 +1,1 @@
-// const url = "https://docs.noroff.dev/ecom-endpoints/rainy-days";
 
-// const getData = async (path) => {
-//   try {
-//     const response = await fetch(`${url}/${path}`);
-//     const json = await response.json();
-//     return json;
-//   } catch (error) {
-//     alert("an error occured!");
-//   }
-// };
-
-// export { getData };
