9399654223e295dd49c164051c8deb161df6a9db
VAbrishamii <vahide.abri@yahoo.com>
2024-02-16 21:57:55 +0100

    Commit

--- script/addToCard.mjs
+++ script/addToCard.mjs
@@ -1,30 +1,53 @@
-import { burgerIcon,crossIcon,burgerMenu } from "./showburgerMenu.mjs";
+import { burgerIcon, crossIcon, burgerMenu } from "./showburgerMenu.mjs";
 import { url } from "./BaseUrl.mjs";
 
 let posts = null;
-const  details = document.querySelectorAll('.details');
-let productId = new URLSearchParams(windiw.location.search).get('id');
+let details = document.querySelector(".details");
+let productId = new URLSearchParams(window.location.search).get("id");
 
+const detailsProducts = (product) => {
+  if (!product) {
+    window.location.href = "/";
+    return;
+  }
+  // details.forEach((details) => {
+  // details.innerHTML = " ";
 
+  // const thisProduct = products.find(value => value.id == productId);
 
-const detailsProducts = (products) => {
-    details.innerHTML = " ";
-}
-
+  details.querySelector(".image img").src = product.image;
+  details.querySelector(".name").innerText = product.title;
+  details.querySelector(".price").innerText = "NOK" + product.price;
+  details.querySelector(".description").innerText = product.description;
+  details.querySelector(".color").innerText = "Color:" + product.baseColor;
+  details.querySelector(".size").innerText = "Choose Your size: " + product.sizes;
+  details.querySelector(".gender").innerText = product.gender;
+  details.querySelector(".discountedprice").innerText = "DiscountedPrice: NOK"+ product.discountedPrice;
+};
 
 async function doFetch(url) {
-    try {
-      const response = await fetch(url);
-      const json = await response.json();
-      return json;
-    } catch (error) {
-      console.log("Error", error);
-      alert("An error ocuured! Please try again");
-    }
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.log("Error", error);
+    alert("An error ocuured! Please try again");
   }
-  
-  async function main() {
+}
+
+async function main() {
+  // posts = await doFetch(url);
+  // detailsProducts(posts);
+  try{
     posts = await doFetch(url);
-    showProducts(posts);
+    console.log(posts);
+    const thisProduct = posts.find(value => value.id == productId);
+    console.log(thisProduct);
+    detailsProducts(thisProduct);
+  }catch(error){
+    console.log("Error fetching data:", error);
+    alert("An error ocuured! Please try again")
   }
-  main()
+}
+main();
